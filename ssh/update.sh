2b698d9b7f5df90aa60df40ea151aaed173f9dd0
GHIFARI160 <ghifari@ghifari160.com>
2025-07-05 10:29:48 -0500

    ssh: Add update script
    Manually updating the configuration and scripts is time consuming and
    error prone. Add a script to do it all.

---
+++ ssh/update.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+UPDATE_SERVER=0
+UPDATE_RENEWER=0
+
+while (( "$#" )); do
+    case "$1" in
+        --all)
+            UPDATE_SERVER=1
+            UPDATE_RENEWER=1
+            shift
+            ;;
+
+        --server)
+            UPDATE_SERVER=1
+            shift
+            ;;
+
+        --renewer)
+            UPDATE_RENEWER=1
+            shift
+            ;;
+
+        *)
+            echo "Unsupported argument $1" >&2
+    esac
+done
+
+update_server () {
+    echo Backing up old server config
+    mv -f /etc/ssh/sshd_config /etc/ssh/sshd_config.old
+
+    echo Downloading updated server config
+    curl -fsSL "https://raw.githubusercontent.com/DiamondDrakeVentures/pki/refs/heads/main/ssh/sshd_config" \
+        -o /etc/ssh/sshd_config
+}
+
+update_renewer () {
+    echo Reading existing renewer config
+    if [[ -f /etc/cron.weekly/rotate-ssh-certificate ]]; then
+        eval "$(grep HC_KEY= /etc/cron.weekly/rotate-ssh-certificate)"
+    fi
+
+    echo Downloading updated renewer
+    curl -fsSL "https://raw.githubusercontent.com/DiamondDrakeVentures/pki/refs/heads/main/ssh/rotate_ssh_certificate" \
+        -o /etc/cron.weekly/rotate-ssh-certificate
+
+    echo Configuring updated renewer
+    if [[ -n "$HC_KEY" ]]; then
+        sed -i "s/HC_KEY=/HC_KEY=$HC_KEY/" /etc/cron.weekly/rotate-ssh-certificate
+    fi
+    chmod 755 /etc/cron.weekly/rotate-ssh-certificate
+}
+
+if [[ $UPDATE_SERVER -eq 1 ]]; then
+    update_server
+fi
+
+if [[ $UPDATE_RENEWER -eq 1 ]]; then
+    update_renewer
+fi
